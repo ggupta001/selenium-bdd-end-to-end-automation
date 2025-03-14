ebd1d98c200e2624b25ac704f275a88bc02dbf6f
gaurav <yourstepcampus@gmail.com>
2025-03-14 23:15:06 +0530

    latest code

--- src/test/java/org/stepcampus/stepdefinitions/LoginPageTest.java
+++ src/test/java/org/stepcampus/stepdefinitions/LoginPageTest.java
@@ -7,27 +7,35 @@ import org.stepcampus.pageobjects.LoginPage;
 import java.io.IOException;
 
 public class LoginPageTest extends WebDriverManagerUtils {
-   public LoginPage loginpg;
+    public LoginPage loginpg;
 
+    // Constructor to initialize the LoginPage object
     public LoginPageTest() throws IOException {
-    loginpg=new LoginPage(driver);
-
+        loginpg = new LoginPage(driver);
     }
+
     @Given("User can see Customer login page")
     public void user_can_see_customer_login_page() {
         loginpg.seeLoginPage();
     }
+
     @When("user enter username and password")
     public void user_enter_username_and_password() {
-        final String userName="MY_API_KEY";
-        final String password="DB_PASSWORD";
-         loginpg.enterUserName(System.getenv(userName));
-         loginpg.enterPassword(System.getenv(password));
+        // Fetch username and password from environment variables
+        final String userName = System.getenv("MY_API_KEY"); // Fetch from environment variable
+        final String password = System.getenv("DB_PASSWORD"); // Fetch from environment variable
+
+        if (userName == null || password == null) {
+            throw new IllegalArgumentException("Username or password is missing from the environment variables");
+        }
+
+        // Pass the values to the login page methods
+        loginpg.enterUserName(userName);
+        loginpg.enterPassword(password);
     }
+
     @Then("user can able to logged in")
     public void user_can_able_to_logged_in() {
         loginpg.clickOnLogin();
     }
-
-
 }
